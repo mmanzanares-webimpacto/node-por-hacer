6bbc613134a90f784bc3ff769501d513da02d433
mmanzanares <mmanzanares@webimpacto.es>
2019-05-07 15:56:26 +0200

    Primer commit

---
+++ README.md
@@ -0,0 +1,7 @@
+##Aplicación de comandos
+
+Este es uno de los ejercicio del curso de Node de Udemy
+
+```
+npm install
+```
---
+++ config/yargs.js
@@ -0,0 +1,27 @@
+const descripcion = {
+    demand: true,
+    alias: 'd'
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+};
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borra una tarea', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
